766a0734fa882e29919d8c6ef66f58fc6be3955a
TaypoH <merkantiln@gmail.com>
2025-06-19 11:08:43 +0300

    Fix Hot Reload

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -46,7 +46,7 @@ const Home = props => {
                 <span className={styleName}>{text}</span>
               </div>
               <p>
-                123Launch a naming contest to engage hundreds of naming experts as
+                Launch a naming contest to engage hundreds of naming experts as
                 you’re guided through our agency-level naming process. Or,
                 explore our hand-picked collection of premium names available
                 for immediate purchase
