7ba107ed78979a0880d2a79df129068ae27529c6
TaypoH <merkantiln@gmail.com>
2025-06-21 15:13:49 +0300

    Add static data to transaction page

--- client/src/pages/TransactionsPage/TransactionsPage.jsx
+++ client/src/pages/TransactionsPage/TransactionsPage.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import Header from '../../components/Header/Header';
 
-function TransactionsPage() {
+function TransactionsPage () {
+  const transactions = [
+    {
+      id: 1,
+      userId: 1,
+      createdAt: '2025-01-01',
+      operationType: 'INCOME',
+      summ: 10,
+    },
+    {
+      id: 2,
+      userId: 1,
+      createdAt: '2025-01-01',
+      operationType: 'INCOME',
+      summ: 10,
+    },
+  ];
+
+  const mapTransactions = t => (
+    <tr key={t.id}>
+      <td>{t.createdAt}</td>
+      <td>{t.operationType}</td>
+      <td>{t.summ}</td>
+    </tr>
+  );
+
   return (
-    <div>TransactionsPage</div>
-  )
+    <>
+      <Header />
+      <main>
+        <table>
+          <caption>Your Transactions</caption>
+          <thead>
+            <tr>
+              <th key={1}>Date</th>
+              <th key={2}>Operation Type</th>
+              <th key={3}>Summ</th>
+            </tr>
+          </thead>
+          <tbody>{transactions.map(mapTransactions)}</tbody>
+        </table>
+      </main>
+    </>
+  );
 }
 
-export default TransactionsPage
+export default TransactionsPage;
