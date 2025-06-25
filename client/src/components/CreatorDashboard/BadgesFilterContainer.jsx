89b27984e1e9fec74f10357cc8878aeaa5c9e503
TaypoH <merkantiln@gmail.com>
2025-06-25 19:22:36 +0300

    Add tests to functions

---
+++ client/src/components/CreatorDashboard/BadgesFilterContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function BadgesFilterContainer ({
+  creatorFilter,
+  changePredicate,
+  badgesParams,
+}) {
+  const mapBadges = b =>
+    creatorFilter[b.name] && (
+      <li key={b.name}>
+        {b.label}
+        <button
+          onClick={() => {
+            changePredicate({
+              name: b.name,
+              value: b.defaultValue,
+            });
+          }}
+        >
+          X
+        </button>
+      </li>
+    );
+
+  return <ul>{badgesParams.map(mapBadges)}</ul>;
+}
+
+export default BadgesFilterContainer;
